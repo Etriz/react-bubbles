09d59f16bf9e2bbf6c6e8cee4611fdf4abd87429
ryanp <ryanpanimation@gmail.com>
2020-06-12 21:13:43 -0500

    able to delete color

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -3,12 +3,21 @@ import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
+  useEffect(() => {
+    axiosWithAuth()
+      .get("/colors")
+      .then((res) => {
+        setColorList([...res.data]);
+      })
+      .catch((err) => console.log(err.message, err.response.data.error));
+  }, []);
   return (
     <>
       <ColorList colors={colorList} updateColors={setColorList} />
